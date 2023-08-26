ff8be34ae5e6c67b889bf62d4682ad76890d0b9e
Githubmagali <106199984+Githubmagali@users.noreply.github.com>
2023-08-26 15:45:53 -0300

    cambios

--- js/efectos.js
+++ js/efectos.js
@@ -7,15 +7,16 @@ $(document).ready(function(){
             top: '0'
         }, 2000 +(index * 500));
     });
-});
 
-$(document).ready(function(){
-    $('.foto img').each(function(index, elemento){
-        $(this).css({
-            'top':'-200px'
-        });
-        $(this).animate({
-            top: '0'
-        }, 200 +(index * 500));
+
+if ( $(window).width() > 800){
+    $('.texto .titulo').css({
+        opacity: 0,
+        marginLeft: 0
     });
+    $('.texto .titulo').animate({
+        opacity: 1,
+        marginLeft: '-52px'
+    }, 1500);
+}
 });
